fb4f87d85697badafeb877db159a6965c565e47c
Renelle Tigue <titingrenelle@gmail.com>
2022-04-08 10:41:48 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// Open and close menu on mobile
+const openMenu = document.querySelector('.header__menu-icon');
+const closeMenu = document.querySelector('.header__close-icon');
+const mobileMenu = document.querySelector('.header__nav-mobile');
+
+openMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('hide');
+    closeMenu.classList.remove('hide');
+    openMenu.classList.add('hide');
+});
+
+closeMenu.addEventListener('click', () => {
+    mobileMenu.classList.add('hide');
+    closeMenu.classList.add('hide');
+    openMenu.classList.remove('hide');
+});
+
+// Set active class on Desktop menu items
+const desktopMenuItems = document.querySelectorAll('.header__nav-desktop li a');
+console.log(desktopMenuItems);
+
+for (let i = 0; i < desktopMenuItems.length; i++) {
+  desktopMenuItems[i].addEventListener("click", function(e) {
+    let current = document.querySelectorAll(".header__nav-desktop li a.active");
+    console.log(current);
+    current[0].classList.remove("active");
+    e.target.classList.add("active");
+  });
+}
+
+// Set active class on Mobile menu items
+const mobileMenuItems = document.querySelectorAll('.header__nav-mobile li a');
+console.log(mobileMenuItems);
+
+for (let i = 0; i < mobileMenuItems.length; i++) {
+  mobileMenuItems[i].addEventListener("click", function(e) {
+    let current = document.querySelectorAll(".header__nav-mobile li a.active");
+    console.log(current);
+    current[0].classList.remove("active");
+    e.target.classList.add("active");
+  });
+}
